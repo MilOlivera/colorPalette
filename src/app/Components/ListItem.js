b5a264b5d75a2930b00a6453e16aa7fa4d81afbb
MilOlivera <mil_olivera@hotmail.com>
2024-02-22 10:44:42 -0300

    favicon

--- src/app/Components/ListItem.js
+++ src/app/Components/ListItem.js
@@ -1,31 +1,31 @@
-import styles from "../styles/page.module.css";
+import styles from '@/app/Styles/page.module.css'
+import { useState } from 'react';
+const copy = <i className='far fa-copy'></i>
 
-const copy = <i className="far fa-copy"></i>;
+const listItem = ({rgb, hex}) => {
 
-function ListItem({ rgb, hex }) {
-  const [copied, setCopied] = useState(false);
+    const [copied, setCopied] = useState(false);
 
-  // Copiar al portapapeles
-  const copyToClipboard = (e) => {
-    const color = e.target.innerText;
-    navigator.clipboard.writeText(color);
-  };
+    const copyToClipboard = (e) => {
+        const color = e.target.innerText;
+        navigator.clipboard.writeText(color);
+    }
 
-  return (
-    <li className={styles.colorName} style={{ background: rgb }}>
-      <span
-        onClick={(e) => {
-          copyToClipboard(e);
-          setCopied(true);
-          setTimeout(() => {
-            setCopied(false);
-          }, 1000);
-        }}
-      >
-        {copied ? "Copiado!" : hex} {copy}
-      </span>
-    </li>
-  );
+    return(
+        <>
+        <li className={styles.colorName} style={{background: rgb}}>
+            <span onClick={(e) => {
+                copyToClipboard(e)
+                setCopied(true);
+                setTimeout(() => {
+                    setCopied(false)
+                }, 1000)
+            }}>
+                {copied ? "Copiado!" : hex} {copy}
+            </span>
+        </li>
+        </>       
+    )
 }
 
-export default ListItem;
+export default listItem;
